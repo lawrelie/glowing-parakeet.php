22b164ce01bb77d5730e6efb88c1e9b0f9a97939
lawrelie <85341666+lawrelie@users.noreply.github.com>
2021-07-22 09:54:17 +0900

    追加：::createChild()

--- src/Lawrelie/GlowingParakeet/Contents/SearchResults/SearchResult.php
+++ src/Lawrelie/GlowingParakeet/Contents/SearchResults/SearchResult.php
@@ -1,6 +1,14 @@
 <?php
 namespace Lawrelie\GlowingParakeet\Contents\SearchResults;
+use DomainException;
 class SearchResult extends \Lawrelie\GlowingParakeet\Contents\Contents {
+    public function createChild(mixed $name, ?string $className = self::class): parent {
+        $result = parent::createChild(['name' => $name], $className);
+        if ($result instanceof self) {
+            return $result;
+        }
+        throw new DomainException;
+    }
     protected function readProperty_queryKey(): string {
         return $this->parakeet->queryKeys['search'];
     }
